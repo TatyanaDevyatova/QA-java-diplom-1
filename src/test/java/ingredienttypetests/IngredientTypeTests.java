c1fbdaf387caee1f02bfb2e6fe6c655b6e65175e
Tatyana Devyatova <tatyana.v.devyatova@gmail.com>
2022-12-13 13:19:32 +0400

    new

--- src/test/java/ingredienttypetests/IngredientTypeTests.java
+++ src/test/java/ingredienttypetests/IngredientTypeTests.java
@@ -6,34 +6,33 @@ import org.junit.runners.Parameterized;
 import praktikum.Ingredient;
 import praktikum.IngredientType;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class IngredientTypeTests {
-    private final IngredientType type;
+    private final String typeValue;
     private final IngredientType expectedType;
 
-    public IngredientTypeTests(IngredientType type, IngredientType expectedType) {
-        this.type = type;
+    public IngredientTypeTests(String typeValue, IngredientType expectedType) {
+        this.typeValue = typeValue;
         this.expectedType = expectedType;
     }
 
     @Parameterized.Parameters(name = "Тестовые данные: {0} | {1}")
     public static Object[][] getBunData() {
         return new Object[][]{
-                {IngredientType.FILLING, IngredientType.valueOf("FILLING")},
-                {IngredientType.SAUCE, IngredientType.valueOf("SAUCE")},
+                {"FILLING", IngredientType.FILLING},
+                {"SAUCE", IngredientType.SAUCE},
         };
     }
 
     @Test
     public void getTypeShowsExpectedEnum() {
         // Arrange & Act
-        Ingredient ingredient = new Ingredient(type, "ingredient", 0.88f);
+        Ingredient ingredient = new Ingredient(IngredientType.valueOf(typeValue), "ingredient", 0.88f);
         IngredientType actualType = ingredient.getType();
 
         // Assert
         assertEquals("Был получен неверный тип ингредиента", expectedType, actualType);
     }
 }
-
